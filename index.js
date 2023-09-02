6f50873dc073f2e84638b8f5e12cb004b271dbf0
Dipesh <topfivetopten.com>
2023-09-02 13:47:10 +0545

    basic array add function created

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("hekl")
+const myLibrary = [
+    {
+        title:"book1",
+        author:"author1",
+        pages:300,
+        read:true
+    },
+    {
+        title:"book2",
+        author:"author2",
+        pages:250,
+        read:false
+    },
+    {
+        title:"book3",
+        author:"author3",
+        pages:100,
+        read:true
+    },
+];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary(title,author,pages,read) {
+    
+    myLibrary.push({
+        title,author,pages,read
+    })
+}
+
+
+
+console.log(myLibrary);
+addBookToLibrary("book4","author4",500,false);
+console.log(myLibrary);
+
